16159cd0092b3355b31e093437698289c84b61cb
cristianchacohn <cristianchacohn@gmail.com>
2024-11-06 14:13:34 +0000

    algunos cambios

--- src/app.js
+++ src/app.js
@@ -6,6 +6,19 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+  function generateExcuse() {
+    const number = Math.floor(Math.random() * (6 - 0) + 0);
+    who;
+  }
+  generateExcuse();
 };
